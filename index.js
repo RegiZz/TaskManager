4c7062bfa86a0095b7a2380caee5c5fa109fcea8
RegiZz <59662344+RegiZz@users.noreply.github.com>
2024-09-16 10:29:21 +0200

    Add files via upload
    juz dziala

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const task = require("/modules/task")
-const user = require("/modules/user")
+const rl = require('readline-sync');
+const task = require("./modules/task")
+const user = require("./modules/user")
 
 var imie = user.imie
 var mail = user.mail
 
 console.log("Witaj " + imie + " twoj mail to: "+ mail);
 
-var coRobic = prompt("Co chcesz zrobic? (dodaj, ukoncz, wyswietl): ")
+var coRobic = rl.question("Co chcesz zrobic? (dodaj, ukoncz, wyswietl): ")
 
 switch(coRobic){
     case "dodaj":
-        var id = prompt("podaj id zadania: ")
-        var opis = prompt("podaj opis zadania: ")
+        var id = rl.question("podaj id zadania: ")
+        var opis = rl.question("podaj opis zadania: ")
         task.dodajZadanie(id,opis,false);
         break;
     case "ukoncz":
-        var id = prompt("podaj id zadania: ")
+        var id = rl.question("podaj id zadania: ")
         task.ukonczZadanie(id)
         break;
     case "wyswietl":
